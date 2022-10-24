fc94707361c82aeb1e329b6a8946e894f24b8c54
Magicredragon <63193630+Magicredragon@users.noreply.github.com>
2022-10-24 03:33:24 +0300

    JavaExercises

---
+++ JavaMyExercises/DrawRainbowTest/src/drawrainbowtest/DrawRainbowTest.java
@@ -0,0 +1,18 @@
+//EXAMPLE FROM BOOK OF DEITEL PAGE 291
+package drawrainbowtest;
+
+import javax.swing.JFrame;
+public class DrawRainbowTest {
+
+   
+    public static void main(String[] args) {
+        DrawRainbow panel = new DrawRainbow();
+        JFrame application = new JFrame();
+        
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        application.add(panel);
+        application.setSize(400,250);
+        application.setVisible(true);
+    }
+    
+}
